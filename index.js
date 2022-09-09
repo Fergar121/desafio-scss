9209b23da4ec4d02ec7e4525578afa4b012cab13
Fermin Garcia <fermingarcia389@gmail.com>
2022-09-08 21:25:55 -0300

    javascript

---
+++ index.js
@@ -0,0 +1,26 @@
+
+let secciones = prompt("Ingrese la seccion: 1- nike 2-Verano 3-rave 4-Buzos");
+ 
+ switch(secciones){
+
+    case 1:
+        alert("Productos nike: Nike Air 70= 6000, Nike Airmax Colors= 5500, Nike Zoom= 4700");
+        break;
+
+    case 2:
+        alert("Productos de Verano: Floops = 3500, Ojotas= 2700, Sandalias=4900");
+        break;
+
+    case 3:
+        alert("Productos Rave = Rave Beige Lisas = 5500, Rave Negra = 6700, Rave gris= 4900");
+        break;
+
+    case 4:
+        alert("Buzos: Buzo Dolce&Gabana = 5000, Buzo Tommy hilfigher= 5000, Buzo Gucci = 5000");
+        break;
+
+    default: 
+    alert("Error al ingresar la seccion")
+ };
+
+
